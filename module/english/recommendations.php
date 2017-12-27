8c20c8e6e8ebe60c34f95e411250f52db6dfa3f1
h我叫小刀 <214564382@qq.com>
2017-12-27 16:34:17 +0800

    english recommendation.php commit

---
+++ module/english/recommendations.php
@@ -0,0 +1,53 @@
+<!--RECOMMENDATIONS TITLE-->
+<h2 id="chat" class="sectionHead">Recommendations</h2>
+<!--RECOMMENDATIONS-->
+<ul id="recommends">
+	<li><h3>Zhengzhou WB Technology Co. Ltd.</h3></li>
+	<li>
+		<div class="details">
+			<h3>Qi Li</h3>
+			<h4>General Manager</h4>
+		</div>
+		<p>"Zhihua Wei's ability to work is very good, very consistent with the development needs of the
+			company."</p>
+	</li>
+	<li>
+		<div class="details">
+			<h3>Zhaoyong Bai</h3>
+			<h4>Project Manager</h4>
+		</div>
+		<p>"His ability to learn is very good, the new technology needed in the development is always fast to learn
+			to apply, and never to delay the time. This is great."</p>
+	</li>
+
+	<li><h3>MW Information Technology Co. Ltd.</h3></li>
+	<li>
+		<div class="details">
+			<h3>Junjie Cao</h3>
+			<h4>Java Engineer</h4>
+		</div>
+		<p>"His logical thinking ability is very good, the programming ability is very strong, and the code is very
+			standard, just like his person."</p>
+	</li>
+
+	<li><h3>Huixin Technology Co. Ltd.</h3></li>
+	<li>
+		<div class="details">
+			<h3>Chungang Li</h3>
+			<h4>Manager</h4>
+		</div>
+		<p>"Zhihua Wei, Professional ability is very strong, the problem of independent thinking is always many
+			aspects. In a word, it is a very good person."</p>
+	</li>
+	<li>
+		<div class="details">
+			<h3>Fengnian Xu</h3>
+			<h4>UI Designer</h4>
+		</div>
+		<p>"He worked very smoothly with him and was able to work well in the team. He is a good friend in his work
+			and life."</p>
+	</li>
+</ul>
+<!--END RECOMMENDATIONS-->
+
+<div class="clear"></div>
