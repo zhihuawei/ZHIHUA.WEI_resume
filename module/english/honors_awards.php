697ecd287169293d1540e2f11f9c2410a234ab43
h我叫小刀 <214564382@qq.com>
2017-12-27 15:36:52 +0800

    english honors_awards.php commit

---
+++ module/english/honors_awards.php
@@ -0,0 +1,13 @@
+<!--HONORS / AWARDS TITLE-->
+<h2 id="ribbon" class="sectionHead">Honors &amp; Awards</h2>
+
+<!--HONORS / AWARDS-->
+<ul id="honorsAwards">
+    <li>Third Award for The fourth "HUAWEI Cup" National College Students' intelligent design contest.</li>
+    <li>First Award for The fourth "RuiZhi Cup" college students in Fujian province and embedded intelligent design contest.</li>
+    <li>Innovation Award for The fourth "RuiZhi Cup" college students in Fujian province and embedded intelligent design contest.</li>
+    <li>Third Award for The fifth Chinese college student service outsourcing innovation and Entrepreneurship Competition.</li>
+</ul>
+<!--END HONORS / AWARDS-->
+
+<div class="clear"></div>
