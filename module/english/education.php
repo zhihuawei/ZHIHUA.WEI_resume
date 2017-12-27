93d647d4983ba66da7ab1c52fdd0f443be3c3444
h我叫小刀 <214564382@qq.com>
2017-12-27 13:50:27 +0800

    english education.php commit

---
+++ module/english/education.php
@@ -0,0 +1,17 @@
+<!--EDUCATION TITLE-->
+<h2 id="learn" class="sectionHead">Education</h2>
+<!--EDUCATION-->
+<ul id="schools">
+    <li>
+        <div class="details">
+            <h3>Longyan University</h3>
+            <h4>Degree Title - Bachelor</h4>
+            <h5>2012 - 2016</h5>
+        </div>
+        <p>Majoring in computer science and technology and obtaining a bachelor's degree. The auxiliary major in the
+            engineering of the Internet of materials is a solid professional knowledge.</p>
+    </li>
+</ul>
+<!--END EDUCATION-->
+
+<div class="clear"></div>
