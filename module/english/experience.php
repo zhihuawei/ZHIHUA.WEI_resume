6f7e274ff1edd24e9b9d1d2f55a97c2fa915d228
h我叫小刀 <214564382@qq.com>
2017-12-27 14:58:37 +0800

    english experience.php commit

---
+++ module/english/experience.php
@@ -0,0 +1,36 @@
+<!--EXPERIENCE TITLE-->
+<h2 id="clock" class="sectionHead">Experience</h2>
+<!--EXPERIENCE-->
+<ul id="jobs">
+    <li>
+        <div class="details">
+            <h3>Zhengzhou WB Technology Co. Ltd.</h3>
+            <h4>PHP Developer - Zhengzhou, Henan</h4>
+            <h5>Apr 2016 - Present</h5>
+        </div>
+        <p>It is mainly engaged in the development of PHP, participating in the needs analysis, database design, project
+            development, demand analysis of the responsible module and the supply of solutions.</p>
+    </li>
+    <li>
+        <div class="details">
+            <h3>MW Information Technology Co. Ltd.</h3>
+            <h4>The Back-End Developer - Xiamen, Fujian</h4>
+            <h5>Oct 2015 - Apr 2016</h5>
+        </div>
+        <p>Responsible for the development and implementation of the company's product technology, design and
+            development of the mobile Internet product function, the project interface and the development of the PC
+            end.</p>
+    </li>
+    <li>
+        <div class="details">
+            <h3>Huixin Technology Co. Ltd.</h3>
+            <h4>Software Engineer - Dongguan, Guangdong</h4>
+            <h5>Jul 2015 - Oct 2015</h5>
+        </div>
+        <p>Responsible for developing, maintaining, implementing and testing the web site system, participating in
+            database design, and developing some of the PHP programs for two times.</p>
+    </li>
+</ul>
+<!--END EXPERIENCE-->
+
+<div class="clear"></div>
