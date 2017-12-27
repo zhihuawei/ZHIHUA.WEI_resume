459103d64e56c9de284301702149d75afe89a74c
h我叫小刀 <214564382@qq.com>
2017-12-27 13:38:58 +0800

    english as_seen_on.php commit

---
+++ module/english/as_seen_on.php
@@ -0,0 +1,55 @@
+<!--AS SEEN ON TITLE-->
+<h2 id="eye" class="sectionHead">As Seen On</h2>
+<!--PROJECT-->
+<ul id="project">
+    <li>
+        <div class="details">
+            <h3>Fuzhong Shop Platform Project</h3>
+            <h4>PC,Android,Ios</h4>
+            <h5>Mar 2017 - Oct 2017</h5>
+        </div>
+        <p>Fuzhong shop platform project is an online business city project, only the mobile terminal App, there is no
+            web browser program. I am mainly responsible for the development of the backstage and mobile App interface,
+            and the project is now online.</p>
+    </li>
+    <li>
+        <div class="details">
+            <h3>Insurance Information Processing System</h3>
+            <h4>PC,Weixin</h4>
+            <h5>Jan 2017 - Feb 2017</h5>
+        </div>
+        <p>The main function of the insurance information processing system is to deal with the information of the
+            insurance application submitted by the user from the WeChat, consent or refusal. This system is my
+            independent responsibility, from demand analysis to project development to the end of the line.
+        </p>
+    </li>
+    <li>
+        <div class="details">
+            <h3>Xinrui Capital System</h3>
+            <h4>PC,Wap</h4>
+            <h5>Jun 2016 - Dec 2016</h5>
+        </div>
+        <p>Xinrui capital system is a stock trading system, stock data acquisition information using data
+            communications. I am mainly responsible for database design, project backstage development, requirements
+            analysis and solutions for their own responsible modules</p>
+    </li>
+</ul>
+<!--END PROJECT-->
+
+<div class="clear"></div>
+
+<!--AS SEEN ON-->
+<ul id="seenOn">
+    <li class="bwWrapper"><img src="images/logos/abduzeedo.jpg" alt="Abduzeedo"/></li>
+    <li class="bwWrapper"><img src="images/logos/logo.png" alt="Awwwards"/></li>
+    <li class="bwWrapper"><img src="images/logos/creattica.png" alt="Creattica"/></li>
+    <li class="bwWrapper"><img src="images/logos/css_mania.png" alt="CSS Mania"/></li>
+    <li class="bwWrapper"><img src="images/logos/design_shack.png" alt="Design Shack"/></li>
+    <li class="bwWrapper"><img src="images/logos/envato_notes.png" alt="Envato Notes"/></li>
+    <li class="bwWrapper"><img src="images/logos/qnt_gallery.png" alt="qnt gallery"/></li>
+    <li class="bwWrapper"><img src="images/logos/unmatchedstyle.png" alt="unmatchedstyle"/></li>
+    <li class="bwWrapper"><img src="images/logos/wptv-logo.png" alt="WordPress TV"/></li>
+</ul>
+<!--END AS SEEN ON-->
+
+<div class="clear"></div>
