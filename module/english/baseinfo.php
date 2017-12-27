000f0e9eeb62c259b45ce1f80efc66eb351a1564
h我叫小刀 <214564382@qq.com>
2017-12-27 13:41:50 +0800

    english baseinfo.php commit

---
+++ module/english/baseinfo.php
@@ -0,0 +1,53 @@
+<!--INTRODUCE NAME-->
+<h2 id="titleName" class="sectionHead">ZHIHUA·WEI</h2>
+
+<!--BASE INFORMATION-->
+<div id="bio">
+	<h2>Full Stack Developer / Programmer</h2>
+	<!--BASE INFORMATION - SOCIAL LINKS-->
+	<div id="socialIcons">
+		<a class="socialIcon" id="phoneIcon" href="#18003829681" title="18003829681" ></a>
+		<a class="socialIcon" id="emailIcon" href="#zhihua_wei@foxmail.com" title="zhihua_wei@foxmail.com"></a>
+		<a class="socialIcon" target="_blank" id="csdnIcon" href="http://blog.csdn.net/zhihua_w" title="CSDN"></a>
+		<a class="socialIcon" target="_blank" id="giteeIcon" href="https://gitee.com/zhihua_wei" title="码云"></a>
+		<a class="socialIcon" target="_blank" id="githubIcon" href="https://github.com/zhihua-wei" title="GitHub"></a>
+		<a class="socialIcon" id="qqIcon" href="#445708162" title="445708162"></a>
+		<a class="socialIcon" target="_blank" id="weiboIcon" href="https://weibo.com/zhihuawei?is_all=1" title="微博"></a>
+
+		<!--MORE ICON OPTIOPS...
+        <a class="socialIcon" target="_blank" id="facebookIcon" href="#"></a>
+        <a class="socialIcon" target="_blank" id="favIcon" href="#"></a>
+        <a class="socialIcon" target="_blank" id="gplusIcon" href="#"></a>
+        <a class="socialIcon" target="_blank" id="youTubeIcon" href="#"></a>
+        -->
+	</div>
+	<!--END SOCIAL LINKS-->
+
+	<!--BASE INFORMATION - PERSONAL INFORMATION-->
+	<p>
+		I'm a web developer and bank-end programmer working and living in Zhengzhou.
+		The full stack developer is my goal and it's my ideal to make programming to change the world.
+		I'm an active author on <a title="CSDN" target="_blank" href="http://blog.csdn.net/zhihua_w">CSDN</a> where i enjoy spending my day in writing technical blog to share.
+		In addition, I have done a lot of <a title="保险信息处理系统" data-rel="prettyPhoto[pp_gal]" href="images/projectPhoto/baoxian.png">projects</a>.
+		Welcome to learn from each other.
+
+		<!--PRODUCTION PHOTO-->
+		<!--IMAGES BELOW ARE HIDDEN AND WILL ONLY APPEAR IN THE GALLERY LINKED TO IN ABOVE PARAGRAPH -->
+		<a title="筑团网项目" class="selectedWork" data-rel="prettyPhoto[pp_gal]"
+		   href="images/projectPhoto/zhutuan.jpg"></a>
+		<a title="信瑞资本股票交易系统" class="selectedWork" data-rel="prettyPhoto[pp_gal]"
+		   href="images/projectPhoto/xinrui.png"></a>
+		<a title="掌上餐厅项目" class="selectedWork" data-rel="prettyPhoto[pp_gal]"
+		   href="images/projectPhoto/zhihuidancan.png"></a>
+		<a title="医美商城平台项目" class="selectedWork" data-rel="prettyPhoto[pp_gal]"
+		   href="images/projectPhoto/yimei.jpg"></a>
+		<a title="医疗之路二期项目" class="selectedWork" data-rel="prettyPhoto[pp_gal]"
+		   href="images/projectPhoto/yiliao.jpg"></a>
+		<a title="养老本项目" class="selectedWork" data-rel="prettyPhoto[pp_gal]"
+		   href="images/projectPhoto/yanglaoben.png"></a>
+	</p>
+	<!--END PERSONAL INFORMATION-->
+</div>
+<!--END BASE INFORMATION-->
+
+<div class="clear"></div>
