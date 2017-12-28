e72a5c94acdcfa77a7c424482c9a35a53aac8c59
h我叫小刀 <214564382@qq.com>
2017-12-28 09:15:22 +0800

    chinese baseinfo.php commit

---
+++ module/chinese/baseinfo.php
@@ -0,0 +1,53 @@
+<!--INTRODUCE NAME-->
+<h2 id="titleName" class="sectionHead">魏志华</h2>
+
+<!--BASE INFORMATION-->
+<div id="bio">
+	<h2>全栈工程师 / 程序员</h2>
+	<!--BASE INFORMATION - SOCIAL LINKS-->
+	<div id="socialIcons">
+		<a class="socialIcon" id="phoneIcon" href="#18003829681" title="18003829681" ></a>
+		<a class="socialIcon" id="emailIcon" href="#zhihua_wei@foxmail.com" title="zhihua_wei@foxmail.com"></a>
+		<a class="socialIcon" target="_blank" id="csdnIcon" href="http://blog.csdn.net/zhihua_w" title="CSDN"></a>
+		<a class="socialIcon" target="_blank" id="giteeIcon" href="https://gitee.com/zhihua_wei" title="码云"></a>
+		<a class="socialIcon" target="_blank" id="githubIcon" href="https://github.com/zhihua-wei" title="GitHub"></a>
+		<a class="socialIcon" id="qqIcon" href="#445708162" title="445708162"></a>
+		<a class="socialIcon" target="_blank" id="weiboIcon" href="https://weibo.com/zhihuawei?is_all=1" title="微博"></a>
+
+		<!--MORE ICON OPTIOPS...
+        <a class="socialIcon" target="_blank" id="facebookIcon" href="#"></a>
+        <a class="socialIcon" target="_blank" id="favIcon" href="#"></a>
+        <a class="socialIcon" target="_blank" id="gplusIcon" href="#"></a>
+        <a class="socialIcon" target="_blank" id="youTubeIcon" href="#"></a>
+        -->
+	</div>
+	<!--END SOCIAL LINKS-->
+
+	<!--BASE INFORMATION - PERSONAL INFORMATION-->
+	<p>
+		我是一个网页开发者，并且也是一个后端程序员，目前工作生活在郑州。
+		全栈工程师是我的目标， 让编程改变世界是我的理想。
+		在 <a title="CSDN" target="_blank" href="http://blog.csdn.net/zhihua_w">CSDN</a> 我是一个很活跃的用户，我很开心在那里可以写我的技术博客分享给大家。
+		除此之外，我还已经做过很多<a title="保险信息处理系统" data-rel="prettyPhoto[pp_gal]" href="images/projectPhoto/baoxian.png">项目</a>。
+		欢迎大家相互学习。
+
+		<!--PRODUCTION PHOTO-->
+		<!--IMAGES BELOW ARE HIDDEN AND WILL ONLY APPEAR IN THE GALLERY LINKED TO IN ABOVE PARAGRAPH -->
+		<a title="筑团网项目" class="selectedWork" data-rel="prettyPhoto[pp_gal]"
+		   href="images/projectPhoto/zhutuan.jpg"></a>
+		<a title="信瑞资本股票交易系统" class="selectedWork" data-rel="prettyPhoto[pp_gal]"
+		   href="images/projectPhoto/xinrui.png"></a>
+		<a title="掌上餐厅项目" class="selectedWork" data-rel="prettyPhoto[pp_gal]"
+		   href="images/projectPhoto/zhihuidancan.png"></a>
+		<a title="医美商城平台项目" class="selectedWork" data-rel="prettyPhoto[pp_gal]"
+		   href="images/projectPhoto/yimei.jpg"></a>
+		<a title="医疗之路二期项目" class="selectedWork" data-rel="prettyPhoto[pp_gal]"
+		   href="images/projectPhoto/yiliao.jpg"></a>
+		<a title="养老本项目" class="selectedWork" data-rel="prettyPhoto[pp_gal]"
+		   href="images/projectPhoto/yanglaoben.png"></a>
+	</p>
+	<!--END PERSONAL INFORMATION-->
+</div>
+<!--END BASE INFORMATION-->
+
+<div class="clear"></div>
