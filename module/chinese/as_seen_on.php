1c365ef631ea2f8a83c6c438d789fcf72db299a5
h我叫小刀 <214564382@qq.com>
2017-12-28 13:50:09 +0800

    chinese as_seen_on.php commit

---
+++ module/chinese/as_seen_on.php
@@ -0,0 +1,48 @@
+<!--AS SEEN ON TITLE-->
+<h2 id="eye" class="sectionHead">项目经验</h2>
+<!--PROJECT-->
+<ul id="project">
+    <li>
+        <div class="details">
+            <h3>蝠众商城平台项目</h3>
+            <h4>PC端后台,Android/Ios APP</h4>
+            <h5>Mar 2017 - Oct 2017</h5>
+        </div>
+        <p>蝠众商城平台项目是一个在线商城项目，只存在移动端App,不存在网页浏览程序。我主要负责后台和移动端App接口的开发，目前项目已上线。</p>
+    </li>
+    <li>
+        <div class="details">
+            <h3>保险信息处理系统</h3>
+            <h4>PC端后台,微信端wap</h4>
+            <h5>Jan 2017 - Feb 2017</h5>
+        </div>
+        <p>保险信息处理系统主要功能是用于处理用户从微信端提交的保险申请信息，同意或是拒绝。此系统是我独立负责，从需求分析到项目开发最后到上线。</p>
+    </li>
+    <li>
+        <div class="details">
+            <h3>信瑞资本股票交易系统</h3>
+            <h4>PC端</h4>
+            <h5>Jun 2016 - Dec 2016</h5>
+        </div>
+        <p>信瑞资本系统是一个股票交易系统，使用通联数据获取股票数据信息。我主要负责数据库设计、项目后台开发、对自己负责模块进行需求分析和解决方案的提供。</p>
+    </li>
+</ul>
+<!--END PROJECT-->
+
+<div class="clear"></div>
+
+<!--AS SEEN ON-->
+<ul id="seenOn">
+    <li class="bwWrapper"><img src="images/logos/abduzeedo.jpg" alt="Abduzeedo"/></li>
+    <li class="bwWrapper"><img src="images/logos/logo.png" alt="Awwwards"/></li>
+    <li class="bwWrapper"><img src="images/logos/creattica.png" alt="Creattica"/></li>
+    <li class="bwWrapper"><img src="images/logos/css_mania.png" alt="CSS Mania"/></li>
+    <li class="bwWrapper"><img src="images/logos/design_shack.png" alt="Design Shack"/></li>
+    <li class="bwWrapper"><img src="images/logos/envato_notes.png" alt="Envato Notes"/></li>
+    <li class="bwWrapper"><img src="images/logos/qnt_gallery.png" alt="qnt gallery"/></li>
+    <li class="bwWrapper"><img src="images/logos/unmatchedstyle.png" alt="unmatchedstyle"/></li>
+    <li class="bwWrapper"><img src="images/logos/wptv-logo.png" alt="WordPress TV"/></li>
+</ul>
+<!--END AS SEEN ON-->
+
+<div class="clear"></div>
