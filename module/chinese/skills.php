e20557e1964f217d2e336abd0cdb5eab86d4b499
h我叫小刀 <214564382@qq.com>
2017-12-28 10:33:46 +0800

    chinese skills.php commit

---
+++ module/chinese/skills.php
@@ -0,0 +1,18 @@
+<!--SKILLS TITLE-->
+<h2 id="tools" class="sectionHead">技能</h2>
+<!--SKILLS-->
+<ul id="skills">
+    <!--Notice: the class names indicate the % of your skills. i.e. s70 = 70%, s40 = 40% etc. -->
+    <li id="skill1" class="s95"><span>PHP</span></li>
+    <li id="skill2" class="s60"><span>Linux</span></li>
+    <li id="skill3" class="s75"><span>Python</span></li>
+    <li id="skill4" class="s90"><span>HTML + CSS</span></li>
+    <li id="skill5" class="s65"><span>jQuery</span></li>
+    <li id="skill6" class="s80"><span>Mysql</span></li>
+    <li id="skill7" class="s70"><span>Apache + Nginx</span></li>
+    <li id="skill8" class="s80"><span>Memcached + Redis</span></li>
+    <li id="skill9" class="s40"><span><del>Photoshop</del></span></li>
+</ul>
+<!--END SKILLS-->
+
+<div class="clear"></div>
