e159924e13027f2b26b672dea0e3765fa43c5775
h我叫小刀 <214564382@qq.com>
2017-12-28 11:32:34 +0800

    chinese honors_awards.php commit

---
+++ module/chinese/honors_awards.php
@@ -0,0 +1,13 @@
+<!--HONORS / AWARDS TITLE-->
+<h2 id="ribbon" class="sectionHead">荣誉 &amp; 奖励</h2>
+
+<!--HONORS / AWARDS-->
+<ul id="honorsAwards">
+    <li>第四届“华为杯”全国大学生智能设计竞赛 三等奖</li>
+    <li>第四届“锐智杯”福建省大学生嵌入式及智能设计大赛 一等奖</li>
+    <li>第四届“锐智杯”福建省大学生嵌入式及智能设计大赛 创新奖</li>
+    <li>第五届中国大学生服务外包创新创业大赛 三等奖</li>
+</ul>
+<!--END HONORS / AWARDS-->
+
+<div class="clear"></div>
