24ba286485adeafd2a51963f637fa5bda77bede4
h我叫小刀 <214564382@qq.com>
2017-12-28 10:56:05 +0800

    chinese experience.php commit

---
+++ module/chinese/experience.php
@@ -0,0 +1,32 @@
+<!--EXPERIENCE TITLE-->
+<h2 id="clock" class="sectionHead">工作经验</h2>
+<!--EXPERIENCE-->
+<ul id="jobs">
+    <li>
+        <div class="details">
+            <h3>郑州 WB 科技有限公司</h3>
+            <h4>PHP 工程师 - 郑州, 河南</h4>
+            <h5>Apr 2016 - 目前</h5>
+        </div>
+        <p>主要从事PHP开发，参与项目中的需求分析、数据库设计、项目开发、对自己负责模块进行需求分析和解决方案的提供等工作。</p>
+    </li>
+    <li>
+        <div class="details">
+            <h3>MW 信息科技有限公司</h3>
+            <h4>后端工程师 - 厦门, 福建</h4>
+            <h5>Oct 2015 - Apr 2016</h5>
+        </div>
+        <p>负责公司产品的技术研发和实现，设计开发移动互联网产品功能，项目接口以及PC端后台的开发。</p>
+    </li>
+    <li>
+        <div class="details">
+            <h3>Huixin 科技有限公司</h3>
+            <h4>软件工程师 - 东莞, 广东</h4>
+            <h5>Jul 2015 - Oct 2015</h5>
+        </div>
+        <p>负责开发、维护、实施及测试网站系统，参与数据库设计工作，对部分PHP程序进行二次开发。</p>
+    </li>
+</ul>
+<!--END EXPERIENCE-->
+
+<div class="clear"></div>
