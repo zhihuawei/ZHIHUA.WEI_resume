a70f30d654d137b000b0a2be940f6ffd08140ec5
h我叫小刀 <214564382@qq.com>
2017-12-28 13:51:39 +0800

    chinese recommendations.php commit

---
+++ module/chinese/recommendations.php
@@ -0,0 +1,48 @@
+<!--RECOMMENDATIONS TITLE-->
+<h2 id="chat" class="sectionHead">同事评语</h2>
+<!--RECOMMENDATIONS-->
+<ul id="recommends">
+	<li><h3>郑州 WB 科技有限公司</h3></li>
+	<li>
+		<div class="details">
+			<h3>李 XX</h3>
+			<h4>经理</h4>
+		</div>
+		<p>"他的工作能力很好，非常符合公司的发展需要。"</p>
+	</li>
+	<li>
+		<div class="details">
+			<h3>白 XX</h3>
+			<h4>项目经理</h4>
+		</div>
+		<p>"他的学习能力很好，开发中需要的新技术总是很快的就能学会应用，更是从来不拖延工期，这个非常棒。"</p>
+	</li>
+
+	<li><h3>MW 信息科技有限公司</h3></li>
+	<li>
+		<div class="details">
+			<h3>曹 XX</h3>
+			<h4>Java 工程师</h4>
+		</div>
+		<p>"他的逻辑思维能力非常好，编程能力也非常强，并且代码非常规范，就像他这个人一样。"</p>
+	</li>
+
+	<li><h3>Huixin 科技有限公司</h3></li>
+	<li>
+		<div class="details">
+			<h3>李 XX</h3>
+			<h4>经理</h4>
+		</div>
+		<p>"专业能力非常强，独立思考问题总是多方面的。总之是一个非常优秀的人。"</p>
+	</li>
+	<li>
+		<div class="details">
+			<h3>徐 XX</h3>
+			<h4>UI 设计师</h4>
+		</div>
+		<p>"和他对接工作非常流畅，团队协作的能力很好。不论在工作中还是生活中他都是一个值得深交的朋友。"</p>
+	</li>
+</ul>
+<!--END RECOMMENDATIONS-->
+
+<div class="clear"></div>
